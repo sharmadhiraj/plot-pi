fd6f2fa58e66d42f2de7e70faeaf2a4bf1f69b62
Dhiraj Sharma <sharmadhiraj.np@gmail.com>
2020-04-27 08:56:07 +0545

    Add output image, reformat code.

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # plot-pi
 Graphical Representation of PI
 
-- Find coordinates of spiral grid
-- Get digit of pi with index of coordinate in spiral
-- Plot a color to coordinate according to digit value (0-9)
+- Find coordinates of spiral grid.
+- Get digit of pi with an index of coordinate in spiral.
+- Plot a color to coordinate according to digit value (0-9).
+<br/>
+
+#### Output
+
+![Output](output.JPG)
--- main.js
+++ main.js
@@ -1,36 +1,36 @@
-var rounds = 130;
-var totalDigits = ((rounds * (rounds + 1)) / 2) * 8 + 1;
+const rounds = 130;
+let totalDigits = ((rounds * (rounds + 1)) / 2) * 8 + 1;
 console.log(totalDigits);
 if (totalDigits > 99999) {
-  totalDigits = 99999;
+    totalDigits = 99999;
 }
-var coordinates = [];
+const coordinates = [];
 
-var x = 0,
-  y = 0,
-  dx = 0,
-  dy = 0,
-  round = 0;
+let x = 0,
+    y = 0,
+    dx = 0,
+    dy = 0,
+    round = 0;
 
 while (coordinates.length < totalDigits) {
-  x = x + dx;
-  y = y + dy;
-  var coordinate = new Coordinate(round, x, y);
-  coordinates.push(coordinate);
+    x = x + dx;
+    y = y + dy;
+    let coordinate = new Coordinate(round, x, y);
+    coordinates.push(coordinate);
 
-  dx = dy = 0;
-  if (!isEndOfRound(coordinate)) {
-    if (isBottom(coordinate)) {
-      dx = 1;
-    } else if (isRight(coordinate)) {
-      dy = 1;
-    } else if (isTop(coordinate)) {
-      dx = -1;
-    } else if (isLeft(coordinate)) {
-      dy = -1;
+    dx = dy = 0;
+    if (!isEndOfRound(coordinate)) {
+        if (isBottom(coordinate)) {
+            dx = 1;
+        } else if (isRight(coordinate)) {
+            dy = 1;
+        } else if (isTop(coordinate)) {
+            dx = -1;
+        } else if (isLeft(coordinate)) {
+            dy = -1;
+        }
+    } else {
+        round++;
+        dy = -1;
     }
-  } else {
-    round++;
-    dy = -1;
-  }
 }
